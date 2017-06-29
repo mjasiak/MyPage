c63d1b424187252fd725a774523261b001e810c1
mjasiak <michal.jasiak148@o2.pl>
2017-06-29 19:04:58 +0200

    Paralax works, about page in progress

--- Scripts/controllers/pageController.js
+++ Scripts/controllers/pageController.js
@@ -1,4 +1,3 @@
 var pageController = angular.module("myApp").controller("pageController",["$scope",function($scope){
-    $scope.hello = "Message from controller!";
     $scope.czas = new Date().toLocaleString();
 }]);
