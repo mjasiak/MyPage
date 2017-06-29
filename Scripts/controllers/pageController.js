d8d99ec3c3f60642e1a43b06ead0cac40f3ba179
Michal <mjasiak@pl.sii.eu>
2017-06-29 15:56:00 +0200

    Working on paralax

--- Scripts/controllers/pageController.js
+++ Scripts/controllers/pageController.js
@@ -1,4 +1,4 @@
 var pageController = angular.module("myApp").controller("pageController",["$scope",function($scope){
-    $scope.hello = "Witaj świecie!";
+    $scope.hello = "Message from controller!";
     $scope.czas = new Date().toLocaleString();
 }]);
